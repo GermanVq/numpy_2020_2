ba754d798b53762e466413d210fa5b684f697312
GermanVq <gavegaq@gmail.com>
2020-09-24 20:39:26 -0500

    Metodos libreria numpy

--- taller.py
+++ taller.py
@@ -32,11 +32,11 @@ print('Total segundos: ', (time.time()-start)*10000)
 
 a = [2, 3, 4, 5, 6]
 b = [7, 9, 10, 11, 12]
-c = [[3, 4, 5], [5, 7, 8], [4, 6 ,7]]
+c = [[3, 4, 5], [5, 7, 8], [4, 6, 7]]
 
 n1 = np.array(a)
 n2 = np.array(b)
-n4 = np.array(c)
+n3 = np.array(c)
 
 # Operaciones
 
@@ -47,6 +47,64 @@ n3 = n1 / n2
 
 # Dimensión de un vector
 
-n3.shape()
+n3.shape
 
 # matriz de ceros
+
+n4 = np.zeros((5, 5))
+
+# matriz de unos
+
+n4 = np.full((4, 5), 7)
+
+# matriz identidad
+
+n4 = np.eye(5)
+
+# matriz con elementos random
+
+n4 = np.random.random((3, 3))
+
+# matriz inversa
+
+np.linalg.inv(n4)
+
+# Elemento mas grande de una matriz
+
+n3.max()
+
+# Elemento mas pequeño de una matriz
+
+n3.min()
+
+# Promedio de una matriz
+
+n3.mean()
+
+# Suma elementos de una matriz
+
+n3.sum()
+
+# Raiz cuadrada de los elementos de una matriz
+
+np.sqrt(n3)
+
+# Seno
+
+np.sin(n3)
+
+# Coseno
+
+np.cos(n3)
+
+# Validar elementos iguales
+
+a = [[3, 4, 5], [5, 7, 8], [4, 6, 7]]
+z = [[3, 4, 5], [5, 7, 8], [4, 6, 7]]
+
+na = np.array(a)
+ns = np.array(z)
+
+na == ns
+
+
